72d3e5b42382edd6ac097ea8597391f170d26e13
DomashevISIP <72748356+DomashevISIP@users.noreply.github.com>
2020-10-13 16:51:12 +0500

    Add files via upload

---
+++ Проект/WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(12, 29);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(400, 550);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.Font = new System.Drawing.Font("Mongolian Baiti", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(150, 157);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Меню игры";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(154, 248);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Выход";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(154, 197);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Старт";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.panel2.Location = new System.Drawing.Point(12, 29);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(400, 550);
+            this.panel2.TabIndex = 1;
+            this.panel2.Visible = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 6);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(322, 2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 21);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "В меню";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(424, 586);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.Text = "Кубики";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Проект/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        int Score = 0;
+
+        static int Col = 8, Row = 15;
+
+        Random RND = new Random();
+
+        Image[] img =
+        {
+            global::WindowsFormsApp2.Properties.Resources.Green1,
+            global::WindowsFormsApp2.Properties.Resources.Grey1,
+            global::WindowsFormsApp2.Properties.Resources.Red1,
+            global::WindowsFormsApp2.Properties.Resources.Yello1,
+        };
+
+        List<Point> Pretindent = new List<Point>();
+
+        PictureBox[,] Pole = new PictureBox[Col, Row];
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Score = 0;
+            panel2.Visible = true;
+            CreatePole();
+            timer1.Enabled = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        void MoverDown()
+        {
+            
+                for (int yy = 0; yy < Row; yy++)
+                {
+                    for (int xx = 0; xx < Col; xx++)
+                    {
+                        if (Pole[xx, yy].Image == null)
+                        {
+                             if (yy + 1 < Row)
+                             {
+                               Pole[xx, yy].Image = Pole[xx, yy + 1].Image;
+                               Pole[xx, yy + 1].Image = null;
+                             }
+                             else
+                             {
+                              Pole[xx, yy].Image = img[RND.Next(img.Length)];
+                             }
+                        }
+                    }
+                }
+            
+        }
+
+        List<Point> IndexList(Point coord)
+        {
+            List<Point> P = new List<Point>();
+
+            Point T;
+
+            //точка с лева
+            if ((coord.X - 1) >= 0) { T = new Point(coord.X - 1, coord.Y); P.Add(T); }
+            //точка с права 
+            if ((coord.X + 1) < Col) { T = new Point(coord.X + 1, coord.Y); P.Add(T); };
+            //точка с верху
+            if ((coord.Y - 1) >= 0) { T = new Point(coord.X, coord.Y - 1); P.Add(T); };
+            // точка с низу
+            if ((coord.Y + 1) < Row) { T = new Point(coord.X, coord.Y + 1); P.Add(T); };
+
+            return P;
+        }
+
+        void TestPole(Point coord, Image Color)
+        {
+            //проверяем есть ли точка в списке
+            int Index = Pretindent.IndexOf(coord);
+            //если точки нет в списке то проверяем
+            if (Index == -1)
+            {
+                //проверяем того ли она цвета
+                if (Pole[coord.X, coord.Y].Image == Color)
+                {
+                    //если она нужного цвета то добавляем ее в список
+                    Pretindent.Add(coord);
+                    //рассчитываем координаты для поиска далее 
+                    List<Point> Test = IndexList(coord);
+                    //создаем для точки набор соседей
+                    foreach (Point T in Test)
+                    {
+                        //проверяем соседей
+                        TestPole(T, Color);
+                    }
+                }
+            }
+        }
+
+        void Click(object sender, EventArgs e)
+        {
+            Pretindent.Clear();
+
+            PictureBox PicClik = sender as PictureBox;
+            Image CurrentPic = PicClik.Image;
+
+
+            string[] NamePars = PicClik.Name.Split('_');
+
+            int _X_ = Convert.ToInt32(NamePars[1]);
+            int _Y_ = Convert.ToInt32(NamePars[2]);
+
+            Point I = new Point(_X_, _Y_);
+
+           List<Point> L = IndexList(I);
+
+            TestPole(I, CurrentPic);
+
+            if (Pretindent.Count > 2)
+
+                foreach (Point ClearPoint in Pretindent)
+                {
+                
+                  Pole[ClearPoint.X, ClearPoint.Y].Image = null;
+
+                }
+                Score += Pretindent.Count;
+
+
+        }
+
+        void CreatePole()
+        {
+            int X = 0, Y = panel2.Height - 50;
+            panel2.Controls.Clear();
+            Pole = new PictureBox[Col, Row];
+
+            for (int yy = 0; yy < Row; yy++)
+            {
+                X = 0;
+                for (int xx = 0; xx < Col; xx++)
+                {
+                    PictureBox pic = new PictureBox()
+                    {
+                        Image = img [RND.Next(img.Length)],
+                        Location = new System.Drawing.Point(X, Y),
+                        Name = "_" + xx + "_" + yy,
+                        Size = new System.Drawing.Size(50, 50),
+                        SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+
+                    };
+
+                    pic.Click += Click;
+
+                    panel2.Controls.Add(pic);
+
+                    Pole[xx, yy] = pic;
+                    X = X + 50;
+                }
+                Y = Y - 50;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            MoverDown();
+            label2.Text = "Набраные очки " + Score;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //показываем меню игры
+            panel1.Visible = true;
+            //прячем игровое поле
+            panel2.Visible = false;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+       
+    }
+}
+
+/*
+            this.pictureBox1.Image = global::WindowsFormsApp2.Properties.Resources.Green;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 482);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+*/
