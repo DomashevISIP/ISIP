b7cb8b9a9f28d04a7570647e22db4ff0a02d6e0c
DomashevISIP <72748356+DomashevISIP@users.noreply.github.com>
2020-10-21 21:33:50 +0500

    Add files via upload

---
+++ Экз/Задание №1/№1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] array = new string[100, 100];
+            FileStream file1 = new FileStream("text1.txt", FileMode.Open);
+            StreamReader reader = new StreamReader(file1);
+            string text = reader.ReadToEnd();
+            FileStream file2 = new FileStream("text2.txt", FileMode.Create);
+            int arrayNumberX = 1; int arrayNumberY = 1;
+            int[] arrayLenghtX = new int[100];
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] != ' ' && text[i] != '\n') array[arrayNumberX, arrayNumberY] += text[i];
+                else
+                if (text[i] == ' ') { arrayNumberX++; arrayLenghtX[arrayNumberY]++; }
+                else { arrayNumberX = 1; arrayNumberY++; }
+            }
+            int arrayLenghtY = arrayNumberY;
+            StreamWriter writer = new StreamWriter(file2);
+            writer.Write(array[1, 1] + " ?");
+
+            for (int j = 2; j <= arrayLenghtX[1] + 1; j++)
+            {
+                writer.Write(" " + array[j, 1]);
+            }
+
+            for (int i = 2; i <= arrayLenghtY; i++)
+            {
+                for (int j = 1; j <= arrayLenghtX[i] + 1; j++)
+                {
+                    if (j != 1) writer.Write(" ");
+                    writer.Write(array[j, i]);
+                }
+            }
+
+            writer.Close();
+            Console.WriteLine("Процесс завершен");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Экз/Задание №2/Задание №2/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace Задание__2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTime = new System.Windows.Forms.Label();
+            this.lblDay = new System.Windows.Forms.Label();
+            this.txtTime = new System.Windows.Forms.TextBox();
+            this.txtDay = new System.Windows.Forms.TextBox();
+            this.btnPrice = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Location = new System.Drawing.Point(79, 76);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(30, 13);
+            this.lblTime.TabIndex = 0;
+            this.lblTime.Text = "Time";
+            // 
+            // lblDay
+            // 
+            this.lblDay.AutoSize = true;
+            this.lblDay.Location = new System.Drawing.Point(79, 126);
+            this.lblDay.Name = "lblDay";
+            this.lblDay.Size = new System.Drawing.Size(26, 13);
+            this.lblDay.TabIndex = 1;
+            this.lblDay.Text = "Day";
+            // 
+            // txtTime
+            // 
+            this.txtTime.Location = new System.Drawing.Point(139, 76);
+            this.txtTime.Name = "txtTime";
+            this.txtTime.Size = new System.Drawing.Size(100, 20);
+            this.txtTime.TabIndex = 2;
+            this.txtTime.TextChanged += new System.EventHandler(this.txtTime_TextChanged);
+            this.txtTime.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtTime_KeyPress);
+            // 
+            // txtDay
+            // 
+            this.txtDay.Location = new System.Drawing.Point(139, 123);
+            this.txtDay.Name = "txtDay";
+            this.txtDay.Size = new System.Drawing.Size(100, 20);
+            this.txtDay.TabIndex = 3;
+            this.txtDay.TextChanged += new System.EventHandler(this.txtDay_TextChanged);
+            this.txtDay.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtDay_KeyPress);
+            // 
+            // btnPrice
+            // 
+            this.btnPrice.Location = new System.Drawing.Point(274, 50);
+            this.btnPrice.Name = "btnPrice";
+            this.btnPrice.Size = new System.Drawing.Size(75, 23);
+            this.btnPrice.TabIndex = 5;
+            this.btnPrice.Text = "Price";
+            this.btnPrice.UseVisualStyleBackColor = true;
+            this.btnPrice.Click += new System.EventHandler(this.btnPrice_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(274, 98);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 6;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(274, 148);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 7;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(454, 226);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnPrice);
+            this.Controls.Add(this.txtDay);
+            this.Controls.Add(this.txtTime);
+            this.Controls.Add(this.lblDay);
+            this.Controls.Add(this.lblTime);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Label lblDay;
+        private System.Windows.Forms.TextBox txtTime;
+        private System.Windows.Forms.TextBox txtDay;
+        private System.Windows.Forms.Button btnPrice;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
